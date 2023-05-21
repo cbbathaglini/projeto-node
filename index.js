c32a77a9b3c936cc41b0a19a9047cd703ff7037a
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-05-20 21:59:15 -0300

    importando chalk

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('olá mundo');
+//const chalk = require('chalk'); //versões anteriores
+import chalk from 'chalk';
+
+
+console.log(chalk.blue('olá mundo'));
+
+console.log('São geralmente recuperados a partir de um ...');
