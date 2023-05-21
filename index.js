9eb8561e93cc3118a417154615df173a8e6d05ed
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-05-21 01:14:40 -0300

    pegando os links do arquivo, criando objeto e passando argumentos

--- index.js
+++ index.js
@@ -13,14 +13,36 @@ function tratamento(mensagem, erro){
 
 async function pegaArquivo(caminhoDoArquivo){
     try{
-    const encode = 'UTF-8';
-    const retorno = await fs.promises.readFile(caminhoDoArquivo, encode);
-    tratamento( retorno,null); 
-    }catch(error){
-        console.log(tratamento(null,error)); 
-    }finally{
-        console.log("Dando erro ou não, sempre cai aqui")
-    }
+        const encode = 'UTF-8';
+        const retorno = await fs.promises.readFile(caminhoDoArquivo, encode);
+        //tratamento( retorno,null); 
+
+        extraiLinks(retorno);
+        }catch(error){
+            console.log(tratamento(null,error)); 
+        }finally{
+           // console.log("Dando erro ou não, sempre cai aqui")
+        }
 }
 
-pegaArquivo("./arquivos/texto2.md")
+
+//encontrar padrão [nomelink](link)
+// \[([^\[]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)
+function extraiLinks(texto){
+    const regex = /\[([^\[]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; //global e multi linha
+    const capturas = [...texto.matchAll(regex)]; //expande o conteúdo iterável OUTRA FORMA while(arr = regex.exec(texto) !== null)
+    //console.log(chalk.yellow(capturas[0][1])); //FileList
+    //console.log(chalk.yellow(capturas[0][2])); //https://developer.mozilla.org/pt-BR/docs/Web/API/FileList
+
+    const resultados = capturas.map(captura => 
+        ({
+            [captura[1]] : captura[2]
+        })
+    );
+    console.log(resultados);
+}
+
+
+//pegaArquivo("./arquivos/texto.md");
+
+export default pegaArquivo;
