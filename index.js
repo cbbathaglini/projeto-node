141a636f3a506b2bea289a91625e18984c7292dc
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-05-20 22:21:19 -0300

    funcao pegaArquivo com tratamento de erro

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function tratamentoErro(erro){
+    throw new Error(chalk.red(erro.code,'Erro ao tentar ler arquivo'));
+}
+
 function pegaArquivo(caminhoDoArquivo){
     const encode = 'UTF-8';
-    fs.readFile(caminhoDoArquivo, encode, (_, encontrado) => {
+    fs.readFile(caminhoDoArquivo, encode, (erro, encontrado) => {
+        if(erro){
+            tratamentoErro(erro);
+        }
         console.log(chalk.green(encontrado));
     });
 }
 
+console.log("Arquivo 1: ");
 pegaArquivo("./arquivos/texto.md");
+console.log("ok \nArquivo 2: ");
+pegaArquivo("./arquivos/texto2.md");
