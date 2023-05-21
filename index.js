93c6c6bd0d261f10c4575385a6a685b150416aec
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2023-05-20 22:45:50 -0300

    funcao com async/await e try catch

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function tratamentoErro(erro){
-    throw new Error(chalk.red(erro.code,'Erro ao tentar ler arquivo'));
+
+
+function tratamento(mensagem, erro){
+    if(erro){
+        throw new Error(chalk.red(erro.code,'Erro ao tentar ler arquivo'));
+    }
+    console.log(chalk.green(mensagem));
 }
 
-function pegaArquivo(caminhoDoArquivo){
-    const encode = 'UTF-8';
-    
-    //promises = código assíncrono
-    fs.promises.readFile(caminhoDoArquivo, encode)
-    .then((texto)=> console.log(chalk.green(texto)))//encadear código assíncrono
-    .catch( (erro) => tratamentoErro(erro));
-     
 
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+    const encode = 'UTF-8';
+    const retorno = await fs.promises.readFile(caminhoDoArquivo, encode);
+    tratamento( retorno,null); 
+    }catch(error){
+        console.log(tratamento(null,error)); 
+    }
 }
 
 pegaArquivo("./arquivos/texto.md")
